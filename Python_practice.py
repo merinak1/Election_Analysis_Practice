5df7d2816913802e7032befeb3e3a271a4456b5c
merinak1 <merina.kansakar@gmail.com>
2021-09-06 19:53:33 -0700

    Adding the election audit code

---
+++ Python_practice.py
@@ -0,0 +1,23 @@
+print("Hello world")
+# print(5+2*3)
+# print(8//5-3)
+# print(8+22*2-4)
+# print(16-3/2+7-1)
+# print(3**3%5)
+# print(5+9*3/2-4)
+# print((5+2)*3)
+# print((8//5)-3)
+# print(8+(22*(2-4)))
+# print(16-3/(2+7)-1)
+# print(3**(3%5))  
+# print(5+(9*3/2-4))
+# print(5+(9*3/(2-4)))
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+dir(int)
